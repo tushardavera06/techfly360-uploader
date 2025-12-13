ff76c3da7507d7e18f6d91745a3c806bafb4a6c6
tushardavera06 <tushardavera06@gmail.com>
2025-12-14 03:30:20 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ ini_set('session.cookie_lifetime', 300);
 session_start();
 
 define('ADMIN_USER', 'tushar');
-define('ADMIN_PASS_HASH', password_hash('tushar@5111', PASSWORD_BCRYPT));
+define('ADMIN_PASS_HASH', password_hash('Tushar@5111', PASSWORD_BCRYPT));
 
 define('MAX_SIZE', 1024 * 1024 * 1024); // 1GB
 define('DRIVE_FOLDER_ID', 'YOUR_FOLDER_ID');
