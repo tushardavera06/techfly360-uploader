6355ec89db293027df783dfc5559df733389a754
tushardavera06 <tushardavera06@gmail.com>
2025-12-16 19:32:35 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,24 +1,21 @@
 <?php
-// ===============================
-// SESSION
-// ===============================
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// ===============================
-// ADMIN LOGIN
-// ===============================
+/* =====================
+   ADMIN LOGIN
+===================== */
 define('ADMIN_USERNAME', 'tushar');
 define('ADMIN_PASSWORD', 'Tushar@5111');
 
-// ===============================
-// TELEGRAM CONFIG
-// ===============================
+/* =====================
+   TELEGRAM CONFIG
+===================== */
 define('BOT_TOKEN', '8370542639:AAFL9iIVbvxPMcqc29Zlg0wQtA3pME4PiQI');
 define('CHANNEL_ID', '-1003582716458');
 
-// ===============================
-// UPLOAD LIMIT (2GB)
-// ===============================
+/* =====================
+   UPLOAD LIMIT (2GB)
+===================== */
 define('MAX_UPLOAD_SIZE', 2 * 1024 * 1024 * 1024);
